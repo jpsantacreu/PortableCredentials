cb40045060cde80beaeb488968c4e2c107f5b52e
jpsantacreu <34453588+jpsantacreu@users.noreply.github.com>
2018-11-27 17:15:52 +0100

    No sourced script needed

--- portable_credentials.sh
+++ portable_credentials.sh
@@ -19,53 +19,56 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 
-#Script must be launched with source to alias use
-if [[ $_ == $0 ]]; then
-  echo "Script must be sourced: source `basename \"$0\"` [upload|download]"
+#Script must be launched with argument
+if [[ $# -eq 0 ]]; then
+  echo "No arguments supplied: ./`basename "$0"` [upload|download]"
   exit 1
 fi
 
-#Default portable credentials configuration file
+# Default portable credentials configuration file
 CONFIG_FILE=~/.portable_credentials
 
-#Default ssh configuration
-DEFAULT_SSH_CONFIG=.ssh/config
+# Default ssh configuration
+SSH_CONFIG_FILE=.ssh/config_portable
 DEFAULT_CERT_DIRECTORY=.ssh/certificates/
 
-#Ssh config file consolidated
-SSH_CONFIG_CONSOLIDATED=~/.ssh/config_consolidated
-
-#Temp files
+# Temp files
 TMP_PATH=/tmp
 TMP_FILE_COMPRESSED=ssh-config.tar.gz
 TMP_FILE_COMPRESSED_ENCRYPTED=$TMP_FILE_COMPRESSED.gpg
 RESPONSE_FILE=$TMP_PATH/tmp_response
 
-#Sed needed
+# Grep needed
+if [[ $GREP_BIN == "" ]]; then
+    BIN_DEPS="$BIN_DEPS grep"
+    GREP_BIN="grep"
+fi
+
+# Sed needed
 if [[ $SED_BIN == "" ]]; then
     BIN_DEPS="$BIN_DEPS sed"
     SED_BIN="sed"
 fi
 
-#Gpg needed
+# Gpg needed
 if [[ $GPG_BIN == "" ]]; then
     BIN_DEPS="$BIN_DEPS gpg"
     GPG_BIN="gpg"
 fi
 
-#Tar needed
+# Tar needed
 if [[ $TAR_BIN == "" ]]; then
     BIN_DEPS="$BIN_DEPS tar"
     TAR_BIN="tar"
 fi
 
-#Curl needed
+# Curl needed
 if [[ $CURL_BIN == "" ]]; then
     BIN_DEPS="$BIN_DEPS curl"
     CURL_BIN="curl"
 fi
 
-#Dependencies check
+# Dependencies check
 which $BIN_DEPS > /dev/null
 if [[ $? != 0 ]]; then
     for i in $BIN_DEPS; do
@@ -76,7 +79,7 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
-#Remove temporary files
+# Remove temporary files
 function remove_temp_files
 {
     if [[ $DEBUG == 0 ]]; then
@@ -88,23 +91,23 @@ function remove_temp_files
     rm -fr "$TMP_PATH/$TMP_FILE_COMPRESSED_ENCRYPTED"
 }
 
-#Check the curl exit code
+# Check the curl exit code
 function check_http_response
 {
     CODE=$?
 
-    #Checking curl exit code
+    # Checking curl exit code
     case $CODE in
-        #OK
+        # OK
         0)
         ;;
-        #Proxy error
+        # Proxy error
         5)
             print "\nError: Couldn't resolve proxy. The given proxy host could not be resolved.\n"
             remove_temp_files
             exit 1
         ;;
-        #Missing CA certificates
+        # Missing CA certificates
         60|58|77)
             print "\nError: cURL is not able to performs peer SSL certificate verification.\n"
             print "Please, install the default ca-certificates bundle.\n"
@@ -126,10 +129,9 @@ function check_http_response
         ;;
     esac
 
-    #Checking response file for generic errors
+    # Checking response file for generic errors
     if grep -q "HTTP/1.1 400" "$RESPONSE_FILE"; then
         ERROR_MSG=$(sed -n -e 's/{"error": "\([^"]*\)"}/\1/p' "$RESPONSE_FILE")
-
         case $ERROR_MSG in
              *access?attempt?failed?because?this?app?is?not?configured?to?have*)
                 echo -e "\nError: The Permission type/Access level configured doesn't match the DropBox App settings!\nPlease run \"$0 unlink\" and try again."
@@ -139,19 +141,19 @@ function check_http_response
     fi
 }
 
-#Compress ssh config and certifies
+# Compress ssh config and certifies
 function compress_data
 {
   $TAR_BIN zcfv $TMP_PATH/$TMP_FILE_COMPRESSED -C ~ $SSH_CONFIG_FILE $CERTS_DIRECTORY #> /dev/null 2>&1
 }
 
-#Encrypt compress file
+# Encrypt compress file
 function encrypt_file
 {
   echo $GPG_PASSPHRASE | $GPG_BIN --output $TMP_PATH/$TMP_FILE_COMPRESSED_ENCRYPTED --passphrase-fd 0 -c $TMP_PATH/$TMP_FILE_COMPRESSED > /dev/null 2>&1
 }
 
-#Discover backend for upload
+# Discover backend for upload
 function db_upload
 {
   case $BACKEND in
@@ -174,11 +176,11 @@ function db_upload
       echo -ne "Backend not recognized\n"
       exit 1
     ;;
-    esac
+  esac;
   remove_temp_files
 }
 
-#Upload to dropbox backend
+# Upload to dropbox backend
 function db_upload_dropbox
 {
     $CURL_BIN -k --progress-bar -X POST -i --globoff -o "$RESPONSE_FILE" --header "Authorization: Bearer $OAUTH_ACCESS_TOKEN" --header "Dropbox-API-Arg: {\"path\": \"/$TMP_FILE_COMPRESSED_ENCRYPTED\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary @"$TMP_PATH/$TMP_FILE_COMPRESSED_ENCRYPTED" https://content.dropboxapi.com/2/files/upload
@@ -194,7 +196,7 @@ function db_upload_dropbox
     fi
 }
 
-#Discover backend for download
+# Discover backend for download
 function db_download
 {
   case $BACKEND in
@@ -217,10 +219,10 @@ function db_download
       echo -ne "Backend not recognized\n"
       exit 1
     ;;
-    esac
+  esac;
 }
 
-#Download to dropbox backend
+# Download to dropbox backend
 function db_download_dropbox
 {
     $CURL_BIN -k -L -s -X POST --globoff -D "$RESPONSE_FILE" -o "$TMP_PATH/$TMP_FILE_COMPRESSED_ENCRYPTED" --header "Authorization: Bearer $OAUTH_ACCESS_TOKEN" --header "Dropbox-API-Arg: {\"path\": \"/$TMP_FILE_COMPRESSED_ENCRYPTED\"}" https://content.dropboxapi.com/2/files/download
@@ -237,13 +239,13 @@ function db_download_dropbox
     fi
 }
 
-#Decrypt compress file
+# Decrypt compress file
 function decrypt_file
 {
   echo $GPG_PASSPHRASE | $GPG_BIN -q --output $TMP_PATH/$TMP_FILE_COMPRESSED --passphrase-fd 0 $TMP_PATH/$TMP_FILE_COMPRESSED_ENCRYPTED
 }
 
-#Descompress ssh config and certifies
+# Descompress ssh config and certifies
 function decompress_data
 {
   $TAR_BIN zxf $TMP_PATH/$TMP_FILE_COMPRESSED -C ~ $SSH_CONFIG_FILE > /dev/null 2>&1
@@ -252,75 +254,68 @@ function decompress_data
   remove_temp_files
 }
 
-#Reconfigure ssh if dont use default config
+# Set include parameter in ssh config
+function set_include
+{
+  $GREP_BIN $SSH_CONFIG_FILE ~/.ssh/config
+  if [[ $? != 0 ]]; then
+    echo "Include $SSH_CONFIG_FILE" >> ~/.ssh/config
+  fi
+}
+
+# Reconfigure ssh if dont use default config
 function ssh_reconfigure
 {
-  #Delete previous alias
-  alias | grep ssh
-  if [[ $? == 0 ]]; then
-    unalias ssh
-  fi
-
-  #if select diferent ssh config file
-  if [[ $SSH_CONFIG_FILE != $DEFAULT_SSH_CONFIG ]];then
-    cat ~/$DEFAULT_SSH_CONFIG $SSH_CONFIG_FILE > $SSH_CONFIG_CONSOLIDATED 
-    alias ssh="ssh -F $SSH_CONFIG_CONSOLIDATED"
-  fi
-
-  # Autocomplete ssh with .ssh/config entrys
+  # Autocomplete ssh with ssh config entrys
   _ssh() 
   {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(grep '^Host' ~/$DEFAULT_SSH_CONFIG ~/$SSH_CONFIG_FILE | awk '{print $2}')
+    opts=$(grep '^Host' ~/$SSH_CONFIG_FILE | awk '{print $2}')
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
       return 0
   }
   complete -F _ssh ssh
 
-  # Autocomplete scp with .ssh/config entrys
+  # Autocomplete scp with ssh config entrys
   _scp() 
   {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(grep '^Host' ~/$DEFAULT_SSH_CONFIG ~/$SSH_CONFIG_FILE | awk '{print $2}')
+    opts=$(grep '^Host' ~/$SSH_CONFIG_FILE | awk '{print $2}')
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
       return 0
   }
   complete -F _scp scp
-
 }
 
 ################
 #### SETUP  ####
 ################
 
-#CHECKING FOR AUTH FILE
+# Checking for auth file
 if [[ -e $CONFIG_FILE ]]; then
 
-    #Loading data
+    # Loading data
     source "$CONFIG_FILE" 2>/dev/null || {
         $SED_BIN -i'' 's/:/=/' "$CONFIG_FILE" && source "$CONFIG_FILE" 2>/dev/null
     }
-    #Checking loaded data
-    if [[ $SSH_CONFIG_FILE = "" ]] || [[ $CERTS_DIRECTORY = "" ]] || [[ $GPG_PASSPHRASE = "" ]] || [[ $BACKEND = "" ]] || [[ $OAUTH_ACCESS_TOKEN = "" ]] ; then
+    # Checking loaded data
+    if [[ $CERTS_DIRECTORY = "" ]] || [[ $GPG_PASSPHRASE = "" ]] || [[ $BACKEND = "" ]] || [[ $OAUTH_ACCESS_TOKEN = "" ]] ; then
         echo -ne "Error loading data from $CONFIG_FILE...\n"
         echo -ne "It is recommended to run $0 unlink\n"
         remove_temp_files
         exit 1
     fi
 
-#NEW SETUP...
+# First execution
 else
 
     echo -ne "\n This is the first time you run this script, please insert following data:\n\n"
-
-    echo -ne " # Ssh configuration file [$DEFAULT_SSH_CONFIG]: "
-    read -r -e -i "$DEFAULT_SSH_CONFIG" SSH_CONFIG_FILE
 
     echo -ne " # Directory of certificates [$DEFAULT_CERT_DIRECTORY]: "
     read -r -e -i "$DEFAULT_CERT_DIRECTORY" CERTS_DIRECTORY
@@ -328,12 +323,12 @@ else
     echo -ne " # Gpg passphrase to securize credential files: "
     read -r GPG_PASSPHRASE
 
-    echo -ne " # Select backend to store credentials (for now only dropbox):\n"
+    echo -ne " # Select backend to store credentials (only Dropbox is available):\n"
     PS3="Choose an option: "
     OPTIONS=("Dropbox" "AWS S3" "Google Drive" "ownCloud")
-    select OPT in "${OPTIONS[@]}"
+    select BACKEND in "${OPTIONS[@]}"
     do
-      case $OPT in
+      case $BACKEND in
         "Dropbox")
           echo -ne " # Dropbox Access token: "
           read -r OAUTH_ACCESS_TOKEN
@@ -361,49 +356,41 @@ else
         exit 1
     fi
 
-    echo "SSH_CONFIG_FILE=$SSH_CONFIG_FILE" >> "$CONFIG_FILE"
     echo "CERTS_DIRECTORY=$CERTS_DIRECTORY" >> "$CONFIG_FILE"
     echo "GPG_PASSPHRASE=$GPG_PASSPHRASE" >> "$CONFIG_FILE"
-    echo "BACKEND=$OPT" >> "$CONFIG_FILE"
+    echo "BACKEND=$BACKEND" >> "$CONFIG_FILE"
     echo "OAUTH_ACCESS_TOKEN=$OAUTH_ACCESS_TOKEN" >> "$CONFIG_FILE"
     echo "   Configuration has been saved."
 
     remove_temp_files
+    set_include
 fi
 
 ################
 #### START  ####
 ################
 
-COMMAND=${*:$OPTIND:1}
-ARG1=${*:$OPTIND+1:1}
-ARG2=${*:$OPTIND+2:1}
+ARGUMENT=${*:$OPTIND:1}
 
-let argnum=$#-$OPTIND
-
-#CHECKING PARAMS VALUES
-case $COMMAND in
+# Checking params values
+case $ARGUMENT in
     upload)
 	compress_data
         encrypt_file
         db_upload
     ;;
-
     download)
         db_download
         decrypt_file
         decompress_data
 	ssh_reconfigure
     ;;
-
     *)
-        if [[ $COMMAND != "" ]]; then
-            print "Error: Unknown command: $COMMAND\n\n"
+        if [[ $ARGUMENT != "" ]]; then
+            print "Error: Unknown argument: $ARGUMENT\n\n"
             ERROR_STATUS=1
         fi
-
     ;;
-
 esac
 
 remove_temp_files
@@ -412,4 +399,4 @@ if [[ $ERROR_STATUS -ne 0 ]]; then
     echo "Some error occured. Please check the log."
 fi
 
-#exit $ERROR_STATUS
+exit $ERROR_STATUS
